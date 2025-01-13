ccb5b4b9b9d2b177ca1f27286462a0ada6906059
Caroline S <165896299+Carolinesnt@users.noreply.github.com>
2025-01-13 15:42:14 +0000

    update

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,28 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const bodyParser = require('body-parser');
-const transactionRoutes = require('./routes/transactionRoutes.js');
-
+const bodyParser = require("body-parser");
+const transactionRoutes = require("./routes/transactionRoutes.js");
 
 const PORT = 5000;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
-mongoose.connect('mongodb://localhost/transactionDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB...'))
-.catch(err => console.error('Could not connect to MongoDB...', err));
+mongoose
+  .connect("mongodb://localhost/transactionDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
-app.use('/api/transactions', transactionRoutes);
+app.use("/api/transactions", transactionRoutes);
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
